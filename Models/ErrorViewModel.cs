ec68f3afa8f1a0954d8b36b71eb80a5c9ad8e5d2
Andrey Jesús Morales Navarro <304560419@uca.ac.cr>
2025-11-13 19:25:04 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Registro_de_Inscripciones___Talleres_de_Tecnología.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
